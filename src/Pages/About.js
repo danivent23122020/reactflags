81e692b3f9c568cd8be94ecd6ae16d131405f4e8
danivent23122020 <dani.vent@orange.fr>
2021-11-23 18:50:36 +0100

    First commit

---
+++ src/Pages/About.js
@@ -0,0 +1,45 @@
+import React from "react";
+import Navigation from "../Components/Navigation";
+
+const About = () => {
+    return (
+        <div>
+            <Navigation />
+            <h1>A propos</h1>
+            <br />
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Est
+                dicta exercitationem explicabo incidunt, facilis atque,
+                consectetur illo nemo praesentium quidem odio quos quod ad optio
+                porro non quasi voluptas repellendus. Quos pariatur
+                exercitationem neque dolore, a quod aliquid quam quia recusandae
+                impedit eaque laboriosam quibusdam libero cupiditate. Itaque
+                corporis atque, aliquid soluta similique eaque dicta ipsum
+                necessitatibus aspernatur tempore laudantium nostrum et quidem
+                ab nisi doloribus tempora inventore exercitationem adipisci unde
+                amet! Ullam officia esse assumenda error rerum perspiciatis
+                possimus voluptatum eligendi magnam sed. Aut accusamus non,
+                veritatis odio debitis nisi sed officiis! Odit maiores cum
+                voluptate eligendi tempora omnis.
+            </p>
+            <br />
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Est
+                dicta exercitationem explicabo incidunt, facilis atque,
+                consectetur illo nemo praesentium quidem odio quos quod ad optio
+                porro non quasi voluptas repellendus. Quos pariatur
+                exercitationem neque dolore, a quod aliquid quam quia recusandae
+                impedit eaque laboriosam quibusdam libero cupiditate. Itaque
+                corporis atque, aliquid soluta similique eaque dicta ipsum
+                necessitatibus aspernatur tempore laudantium nostrum et quidem
+                ab nisi doloribus tempora inventore exercitationem adipisci unde
+                amet! Ullam officia esse assumenda error rerum perspiciatis
+                possimus voluptatum eligendi magnam sed. Aut accusamus non,
+                veritatis odio debitis nisi sed officiis! Odit maiores cum
+                voluptate eligendi tempora omnis.
+            </p>
+        </div>
+    );
+};
+
+export default About;
---
+++ src/Pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navigation from "../Components/Navigation";
+
+const Home = () => {
+    return (
+        <>
+            <Navigation />
+            <h1>Accueil</h1>
+        </>
+    );
+};
+
+export default Home;
