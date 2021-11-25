8f007015f1275b8e00c2023fba3cbb9914a0d931
danivent23122020 <dani.vent@orange.fr>
2021-11-25 16:51:07 +0100

    Triage

--- src/Components/Countries.js
+++ src/Components/Countries.js
@@ -12,21 +12,45 @@ const Countries = () => {
      * triage des pays par population
      */
     const [sortedData, setSortedData] = useState([]);
+
+    /**
+     * éviter boucle infinie
+     */
+    const [playOnce, setPlayOnce] = useState(true);
+
     /**
      * useEffect avec axios pour éviter les requêtes sans fin
      */
     useEffect(() => {
-        axios
-            .get(
-                "https://restcountries.com/v2/all?fields=name,population,region,capital,flag"
-            )
-            .then((result) => setData(result.data));
-    }, []);
+        if (playOnce) {
+            axios
+                .get(
+                    "https://restcountries.com/v2/all?fields=name,population,region,capital,flag"
+                )
+                .then((result) => {
+                    setData(result.data);
+                    setPlayOnce(false);
+                });
+        }
+
+        // fonction triage des pays
+        const sortedCountry = () => {
+            const countryObj = Object.keys(data).map((i) => data[i]);
+            const sortedArray = countryObj.sort((a, b) => {
+                return b.population - a.population;
+            });
+            // on limite le nombre de flags sur la page
+            sortedArray.length = 30;
+            setSortedData(sortedArray);
+        };
+        sortedCountry();
+        // callback data pour rejouer le useEffect
+    }, [data]);
     return (
         <div className="countries">
             <h1>Countries </h1>
             <ul className="countries-list">
-                {data.map((country) => (
+                {sortedData.map((country) => (
                     <Card country={country} key={country.name} />
                 ))}
             </ul>
